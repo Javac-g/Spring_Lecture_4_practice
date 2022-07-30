15fd086d9916c1bc5a0484fb7b67109d0ce89ee9
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-07-30 20:41:55 +0300

    NextOne

--- Services/Service_A.java
+++ Services/Service_A.java
@@ -1,13 +1,14 @@
 package com.company.Services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service(value = "Service_A")
 
-public class Service_A {
-
+public class Service_A implements myService {
+    @Value("${prop.A}")
     private String one;
-
+    @Override
     public String getOne(){
         return one;
     }
--- Services/Service_B.java
+++ Services/Service_B.java
@@ -1,10 +1,13 @@
 package com.company.Services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service(value = "Service_B")
-public class Service_B {
+public class Service_B implements myService {
+    @Value("${prop.C}")
     private String one;
+    @Override
     public String getOne(){
         return one;
     }
