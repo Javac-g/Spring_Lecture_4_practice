bb8b1484f71874e0adf0d7a7757f7384826a94f5
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-07-30 20:25:11 +0300

    service_b anno

--- Services/Service_B.java
+++ Services/Service_B.java
@@ -1,5 +1,8 @@
 package com.company.Services;
 
+import org.springframework.stereotype.Service;
+
+@Service(value = "Service_B")
 public class Service_B {
     private String one;
     public String getOne(){
