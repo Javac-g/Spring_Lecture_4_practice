5e09308e0fdc08e2ecd69e3d2737eec1c52cee39
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-07-30 18:53:35 +0300

    App

---
+++ App.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String...args){
+
+    }
+}
