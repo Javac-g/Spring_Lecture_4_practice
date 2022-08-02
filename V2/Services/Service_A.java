d2935f972ce3666e9ff13d6b52a3c54afdbcedfc
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-08-02 21:05:50 +0300

    Update

--- V2/Services/Service_A.java
+++ V2/Services/Service_A.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service(value = "Service_A")
-public class Service_A {
+public class Service_A implements MyService {
     @Value("${prop.D}")
     private String one;
+    @Override
+    public String getOne(){
+        return one;
+    }
 
 }
