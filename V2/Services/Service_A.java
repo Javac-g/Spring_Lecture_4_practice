56bb3a5d35be2345aa41c55417765f826659bb07
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-08-02 21:45:56 +0300

    Scope proxyMode

--- V2/Services/Service_A.java
+++ V2/Services/Service_A.java
@@ -1,9 +1,12 @@
 package com.company.V2.Services;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 
 @Service(value = "Service_A")
+@Scope(value = "prototype",proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class Service_A implements MyService {
     @Value("${prop.D}")
     private String one;
--- V2/Services/Service_B.java
+++ V2/Services/Service_B.java
@@ -2,10 +2,13 @@ package com.company.V2.Services;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 
 @Primary
 @Service(value = "Service_B")
+@Scope(value = "prototype",proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class Service_B implements MyService {
 
     @Value("${prop.E}")
