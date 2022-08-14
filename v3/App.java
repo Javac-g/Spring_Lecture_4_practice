3c8e8ef27be5a476c306418f7d78de4fedf94964
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-08-14 20:09:20 +0300

    App

---
+++ v3/App.java
@@ -0,0 +1,14 @@
+package com.company.v3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class App {
+    public static void main(String...args){
+
+        SpringApplication.run(App.class,args);
+
+    }
+}
