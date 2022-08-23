d7f23754606d0481a827fa91e293a17adf22f61c
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-08-23 16:19:13 +0300

    Service_C

--- V5/Services/Service_C.java
+++ V5/Services/Service_C.java
@@ -1,17 +1,14 @@
 package com.company.V5.Services;
 
-
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 
-@Service("Servise_B")
+@Service("Service_C")
 @Scope(value = "prototype",proxyMode = ScopedProxyMode.TARGET_CLASS)
-@Primary
-public class Servise_B {
-    @Value("${prop.A}")
+public class Service_C {
+    @Value("${prop.B}")
     private String one;
 
     public String getOne() {
